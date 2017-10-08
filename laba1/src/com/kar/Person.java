2e784c06003d22ef7b4fe5b6705bebc09d669b9a
Nestea <Nestea@DESKTOP-OJGVBPN>
2017-10-08 19:59:58 +0300

    ))))))0

---
+++ laba1/src/com/kar/Person.java
@@ -0,0 +1,13 @@
+package com.kar;
+
+public class Person {
+	public static void main(String[] args) {
+		Object[] arr = new Object[3];
+	    stu[0] = new student;
+	    stu[1] = new student;
+	    stu[2] = new student;
+	    for (int i = 0; i < arr.length; i++) {
+	        System.out.println(arr[i].toString());
+    }
+   
+
